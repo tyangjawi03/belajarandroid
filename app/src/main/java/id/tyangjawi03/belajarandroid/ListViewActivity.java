d08a0a4dc7ca871797f973ff35279ca68647fef6
bashir arrohman <bashir.arrohman@lemsaneg.go.id>
2017-07-07 11:19:57 +0700

    add ListViewActivity

---
+++ app/src/main/java/id/tyangjawi03/belajarandroid/ListViewActivity.java
@@ -0,0 +1,14 @@
+package id.tyangjawi03.belajarandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_view);
+    }
+
+}
