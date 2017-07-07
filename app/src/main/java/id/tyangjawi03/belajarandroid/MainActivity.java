f60d42fb41e42d18341b8ca5694adf55b12e6c51
bashir arrohman <bashir.arrohman@lemsaneg.go.id>
2017-07-07 09:31:06 +0700

    create first activity

---
+++ app/src/main/java/id/tyangjawi03/belajarandroid/MainActivity.java
@@ -0,0 +1,13 @@
+package id.tyangjawi03.belajarandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
