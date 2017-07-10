8580a21a4e98cb390b89270b76967112a5763322
bashir arrohman <bashir.arrohman@lemsaneg.go.id>
2017-07-10 07:51:13 +0700

    create method to handle menu

--- app/src/main/java/id/tyangjawi03/belajarandroid/ListViewActivity.java
+++ app/src/main/java/id/tyangjawi03/belajarandroid/ListViewActivity.java
@@ -4,6 +4,9 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -53,4 +56,28 @@ public class ListViewActivity extends AppCompatActivity {
         }
 
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.menu_satu :
+                Toast.makeText(this, "Menu 1", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menu_dua :
+                Toast.makeText(this, "Menu 2", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        return true;
+    }
+
 }
