fdf816f2969a2c63b1cdbca07e86517c328fec59
bashir arrohman <bashir.arrohman@lemsaneg.go.id>
2017-07-11 08:21:34 +0700

    implement SQLiteOpenHelper

--- app/src/main/java/id/tyangjawi03/belajarandroid/model/MySQLiteHelper.java
+++ app/src/main/java/id/tyangjawi03/belajarandroid/model/MySQLiteHelper.java
@@ -10,18 +10,30 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class MySQLiteHelper extends SQLiteOpenHelper {
 
-    public MySQLiteHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+    private static final String DATABASE_NAME = "Database_Name";
+    private static final int DATABASE_VERSION = 1;
+
+    private String TABLE_NAME = "table_name";
+    private String COLUMN_ID = "id";
+    private String COLUMN_TITLE = "title";
+    private String COLUMN_SUB_TITLE = "sub_title";
+
+    public MySQLiteHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        db.execSQL("CREATE TABLE " + TABLE_NAME + "(" +
+                COLUMN_ID + " INTEGER PRIMARY KEY AUTOCORRECT, " +
+                COLUMN_TITLE + " TEXT NOT NULL, " +
+                COLUMN_SUB_TITLE + " TEXT NOT NULL);");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
     }
-    
+
 }
